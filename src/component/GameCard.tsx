f056088217e5ec4df3e21480f3d3e6d2a22d976c
Keerthi Vasan.J <keerthiv741@gmail.com>
2026-01-07 11:54:09 +0530

    optimized the image with proper dimmension

--- src/component/GameCard.tsx
+++ src/component/GameCard.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, Heading,  HStack,  Image } from
 import type {Game} from "../Hooks/useGame";
 import PlatformIcons from "./PlatformIcon";
 import CriticScore from "./CriticScore";
+import CroppedImageUrl from "./services/ImageUrl";
 
 interface Props {
   game: Game;
@@ -10,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={ CroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={'space-between'}>
